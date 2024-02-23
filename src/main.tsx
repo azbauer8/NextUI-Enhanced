4e43126407605a5f335a7029736b7e21b47ba275
Zach Bauer <azbauer8@gmail.com>
2024-02-22 21:30:57 -0500

    switched to biome for linting

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import Layout from "@/layout/index.tsx"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+// biome-ignore lint/style/noNonNullAssertion: this is fine :)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Layout>
